892df25fe625153c4c71ddee9b6bc37fa4586734
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-07-29 16:40:02 -0300

    GameManager: Gestionar Mapa y Jugador
    - Ahora el Gamenager es responsable de generar el mapa y spawnear al jugador
    - Se remueven referencias de MapaManager relacionadas con el jugador

--- Roguelike 2D/Assets/Scripts/GameManager.cs
+++ Roguelike 2D/Assets/Scripts/GameManager.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private JugadorController jugador;
+    
+    [SerializeField]
+    private MapaManager mapa;
+
+    private TurnosManager turnosManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        turnosManager = new TurnosManager();
+        mapa.GenerarMapa();
+        jugador.Spawn(mapa, new Vector2Int(2,2));
     }
 }
