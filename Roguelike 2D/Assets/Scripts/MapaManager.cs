9c1492633e189a62b6b15630ae1b51807d24d314
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-07-29 11:13:53 -0300

    Generar Paredes
    Refactor Metodo Generar Mapa para incluir Tiles de Paredes

--- Roguelike 2D/Assets/Scripts/MapaManager.cs
+++ Roguelike 2D/Assets/Scripts/MapaManager.cs
@@ -14,6 +14,9 @@ public class MapaManager : MonoBehaviour
     [SerializeField]
     private Tile[] sueloTiles;
     
+    [SerializeField]
+    private Tile[] paredTiles;
+    
     private Tilemap mapaTilemap;
 
     private void Awake()
@@ -23,18 +26,29 @@ public class MapaManager : MonoBehaviour
 
     void Start()
     {
-        GenerarSuelo();
+        GenerarMapa();
     }
 
-    private void GenerarSuelo()
+    private void GenerarMapa()
     {
         for (int y = 0; y < alto; y++)
         {
             for (int x = 0; x < ancho; x++)
             {
-                int indiceTile = Random.Range(0, sueloTiles.Length);
-                mapaTilemap.SetTile(new Vector3Int(x,y,0) , sueloTiles[indiceTile]);
+                Tile tile;
+
+                if (x == 0 || y == 0 || x == ancho - 1 || y == alto - 1)
+                {
+                    tile = paredTiles[Random.Range(0,paredTiles.Length)];
+                }
+                else
+                {
+                    tile = sueloTiles[Random.Range(0, sueloTiles.Length)];
+                }
+                
+                mapaTilemap.SetTile(new Vector3Int(x,y,0) , tile);
             }
         }
     }
+
 }
